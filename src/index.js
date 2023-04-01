e7089819053bfaa4db45ab5d9093d5acfa198684
oleksii-zviezdin <oleksii.zviezdin@gmail.com>
2023-04-01 12:38:52 +0200

    fixrd task 10

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import { getRefs } from './css/refs';
 import { fetchCountries } from './fetchCountries';
 const debounce = require('lodash.debounce')
 
-const refs = getRefs();
-
+const REFS = getRefs();
 const DEBOUNCE_DELAY = 300;
 
-refs.inputEl.addEventListener(`input`, debounce(onSearch, DEBOUNCE_DELAY))
+REFS.inputEl.addEventListener(`input`, debounce(onSearch, DEBOUNCE_DELAY))
 
 function onSearch(e) {
     e.preventDefault();
-    const searchCountry = refs.inputEl.value.trim();
+    const searchCountry = REFS.inputEl.value.trim();
     
     fetchCountries(searchCountry).then(data => {
         if (data.length > 10) {
             Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
-            refs.countryList.innerHTML = ``;
+            REFS.countryList.innerHTML = ``;
             return;
         } else if (data.length >= 2 && data.length <= 10) {
             return renderListInfoAboutCountries(data);
@@ -26,9 +25,9 @@ function onSearch(e) {
         .catch(error => Notiflix.Notify.failure(`Oops, there is no country with that name`))
         .finally(() => {
             function checkForm() {
-                if (refs.inputEl.value.length < 1) {
+                if (REFS.inputEl.value.length < 1) {
                     Notiflix.Notify.info(`Enter the name of the country`)
-                    refs.countryList.innerHTML = ``;
+                    REFS.countryList.innerHTML = ``;
                 }
             }           
             setTimeout(checkForm, 500);
@@ -37,20 +36,20 @@ function onSearch(e) {
 
 function renderCardInfoAboutCountries(country){
             console.log(country)
-    refs.countryList.innerHTML = ``;
+    REFS.countryList.innerHTML = ``;
     const markup = country.map(({ name, flags, languages, capital, population }) =>
-        `<div><img width="20px" style="margin-right: 10px" src="${flags.svg}" alt="Flag of ${name.official}"><span style="font-size: 24px; font-weight: 700">${name.official}</span><div><b>Capital: </b>${capital}</div><div><b>Population: </b>${population}</div></div><div><b>Languages: </b>${Object.values(languages)}</div>`).join("");
-    refs.countryInfo.innerHTML = markup;
-    refs.countryInfo.children.style.display = `flex`;
+        `<div style="display: flex;"><img width="40px" height="auto" style="margin-right: 10px" src="${flags.svg}" alt="Flag of ${name.official}"><span style="font-size: 24px; font-weight: 700">${name.official}</span></div><div style="margin-top: 10px"><b>Capital: </b>${capital}</div><div><b>Population: </b>${population}</div></div><div><b>Languages: </b>${Object.values(languages)}</div>`).join("");
+    REFS.countryInfo.innerHTML = markup;
+    REFS.countryInfo.children.style.display = `flex`;
 
 }
 
 function renderListInfoAboutCountries(countries) {
             console.log(countries)
-    refs.countryInfo.innerHTML = ``;
-    const markup = countries.map(({name, flags}) => `<li><img width="20px" style="margin-right: 10px" src="${flags.svg}" alt="Flag of ${name.official}"><span style="font-weight: 700">${name.common}</span></li>`).join("");
-        refs.countryList.innerHTML = markup;
-        refs.countryList.style.listStyle = "none";
-        refs.countryList.style.margin = "0";
-        refs.countryList.style.padding = "0";
+    REFS.countryInfo.innerHTML = ``;
+    const markup = countries.map(({name, flags}) => `<li><img width="25px" style="margin-right: 10px;" src="${flags.svg}" alt="Flag of ${name.official}"><span style="font-weight: 700">${name.common}</span></li>`).join("");
+        REFS.countryList.innerHTML = markup;
+        REFS.countryList.style.listStyle = "none";
+        REFS.countryList.style.margin = "0";
+        REFS.countryList.style.padding = "0";
 }
